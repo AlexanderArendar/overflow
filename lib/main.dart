7a5380dce52a3bfae89b6a10e649d0ff11119c44
Alexander Arendar <alexander.arendar@gmail.com>
2019-06-26 20:12:53 +0300

    custom painter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'dart:async';
-import 'dart:ui' show window;
-
+import 'dart:ui';
+import 'dart:math';
+import 'dart:ui' as ui;
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -18,80 +19,124 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MapPainter extends CustomPainter {
+  final ui.Image image;
+  final double zoomLevel;
+  final Offset centerOffset;
+
+  MapPainter(this.image, this.zoomLevel, this.centerOffset);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    double pixelRatio = window.devicePixelRatio;
+    Size sizeInDevicePixels = Size(size.width * pixelRatio, size.height * pixelRatio);
+    Paint paint = Paint();
+    paint.style = PaintingStyle.fill;
+    Offset centerOffsetInDevicePixels = centerOffset.scale(pixelRatio / zoomLevel, pixelRatio / zoomLevel);
+    Offset centerInDevicePixels = Offset(image.width / 2, image.height / 2).translate(centerOffsetInDevicePixels.dx, centerOffsetInDevicePixels.dy);
+    Offset topLeft = centerInDevicePixels.translate(-sizeInDevicePixels.width / (2 * zoomLevel), -sizeInDevicePixels.height / (2 * zoomLevel));
+    Offset rightBottom = centerInDevicePixels.translate(sizeInDevicePixels.width / (2 * zoomLevel), sizeInDevicePixels.height / (2 * zoomLevel));
+    canvas.drawImageRect(
+      image,
+      Rect.fromPoints(topLeft, rightBottom),
+      Rect.fromPoints(Offset(0, 0), Offset(size.width, size.height)),
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
 class ImageViewportState extends State<ImageViewport> {
-  double _mapWidth;
-  double _mapHeight;
-  double _alignmentX;
-  double _alignmentY;
-  Image _map;
+  double _zoomLevel;
+  ui.Image _image;
+  bool _resolved;
+  Offset _centerOffset;
+  double _maxHorizontalDelta;
+  double _maxVerticalDelta;
+  Offset _normalized;
+  bool _denormalize = false;
+
+  double abs(double value) {
+    return value < 0 ? value * (-1) : value;
+  }
 
   @override
   void initState() {
     super.initState();
-    _map = widget.map;
-    _mapWidth = widget.width;
-    _mapHeight = widget.height;
-    _alignmentX = 0;
-    _alignmentY = 0;
+    _zoomLevel = widget.zoomLevel;
+    _resolved = false;
+    _centerOffset = Offset(0, 0);
   }
 
   @override
-  void didUpdateWidget(ImageViewport oldWidget){
-    _map = widget.map;
-    _mapWidth = widget.width;
-    _mapHeight = widget.height;
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    ImageStream stream = Image.asset("assets/map.jpg").image.resolve(createLocalImageConfiguration(context));
+    stream.addListener((info, _) {
+      _image = info.image;
+      _resolved = true;
+      setState(() {});
+    });
+  }
+
+  @override
+  void didUpdateWidget(ImageViewport oldWidget) {
+    double normalizedDx = _maxHorizontalDelta == 0 ? 0 : _centerOffset.dx / _maxHorizontalDelta;
+    double normalizedDy = _maxVerticalDelta == 0 ? 0 : _centerOffset.dy / _maxVerticalDelta;
+    _normalized = Offset(normalizedDx, normalizedDy);
+    _denormalize = true;
+    _zoomLevel = widget.zoomLevel;
   }
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        return GestureDetector(
-          onPanUpdate: (DragUpdateDetails updateDetails) {
-            double oldAlignmentX = _alignmentX;
-            double oldAlignmentY = _alignmentY;
-            double xDelta = updateDetails.delta.dx;
-            double xShift = (xDelta / _mapWidth);
-            if (_alignmentX - xShift < -1)
-              _alignmentX = -1;
-            else if (_alignmentX - xShift > 1)
-              _alignmentX = 1;
-            else
-              _alignmentX -= xShift;
-            double yDelta = updateDetails.delta.dy;
-            double yShift = (yDelta / _mapHeight);
-            if (_alignmentY - yShift < -1)
-              _alignmentY = -1;
-            else if (_alignmentY - yShift > 1)
-              _alignmentY = 1;
-            else
-              _alignmentY -= yShift;
-            if (_alignmentX != oldAlignmentX || _alignmentY != oldAlignmentY) setState(() {
-            });
-          },
-          child: OverflowBox(
-            child: _map,
-            minHeight: constraints.maxHeight,
-            minWidth: constraints.maxWidth,
-            maxHeight: double.infinity,
-            maxWidth: double.infinity,
-            alignment: Alignment(_alignmentX, _alignmentY),
-          ),
-        );
-      },
-    );
+
+    void handleDrag(DragUpdateDetails updateDetails){
+      Offset newOffset = _centerOffset.translate(-updateDetails.delta.dx, -updateDetails.delta.dy);
+      if (abs(newOffset.dx) <= _maxHorizontalDelta && abs(newOffset.dy) <= _maxVerticalDelta)
+        setState(() {
+          _centerOffset = newOffset;
+        });
+    }
+    return _resolved
+        ? LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              double actualImageWidth = (_image.width / window.devicePixelRatio) * _zoomLevel;
+              double actualImageHeight = (_image.height / ui.window.devicePixelRatio) * _zoomLevel;
+              double viewportWidth = min(constraints.maxWidth, actualImageWidth);
+              double viewportHeight = min(constraints.maxHeight, actualImageHeight);
+              _maxHorizontalDelta = (actualImageWidth - viewportWidth) / 2;
+              _maxVerticalDelta = (actualImageHeight - viewportHeight) / 2;
+              bool reactOnHorizontalDrag = _maxHorizontalDelta > _maxVerticalDelta;
+              bool reactOnPan = (_maxHorizontalDelta > 0 && _maxVerticalDelta > 0);
+              if (_denormalize) {
+                _centerOffset = Offset(_maxHorizontalDelta * _normalized.dx, _maxVerticalDelta * _normalized.dy);
+                _denormalize = false;
+              }
+              return GestureDetector(
+                onPanUpdate: reactOnPan ? handleDrag : null,
+                onHorizontalDragUpdate: reactOnHorizontalDrag && !reactOnPan ? handleDrag : null,
+                onVerticalDragUpdate: !reactOnHorizontalDrag && !reactOnPan ? handleDrag : null,
+                child: CustomPaint(
+                  size: Size(viewportWidth, viewportHeight),
+                  painter: MapPainter(_image, _zoomLevel, _centerOffset),
+                ),
+              );
+            },
+          )
+        : SizedBox();
   }
 }
 
 class ImageViewport extends StatefulWidget {
-  final Image map;
-  final double width;
-  final double height;
+  final double zoomLevel;
 
   ImageViewport({
-    @required this.map,
-    @required this.width,
-    @required this.height,
+    @required this.zoomLevel,
   });
 
   @override
@@ -100,49 +145,19 @@ class ImageViewport extends StatefulWidget {
 
 class ZoomContainerState extends State<ZoomContainer> {
   double _zoomLevel;
-  Image _image;
-  Completer<ImageInfo> _completer;
-
-  void _init() {
-    _image = Image.asset(
-      "assets/map.jpg",
-      scale: _zoomLevel,
-    );
-    _completer = Completer<ImageInfo>();
-    ImageStream stream = _image.image.resolve(createLocalImageConfiguration(context));
-    stream.addListener((ImageInfo info, _) {
-      _completer.complete(info);
-    });
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _init();
-  }
 
   @override
   void initState() {
     super.initState();
     _zoomLevel = 1;
-    print("device pixel ratio: ${window.devicePixelRatio}");
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        FutureBuilder<ImageInfo>(
-          future: _completer.future,
-          builder: (BuildContext context, AsyncSnapshot<ImageInfo> snapshot) {
-            return (snapshot.data == null || snapshot.connectionState != ConnectionState.done)
-                ? SizedBox()
-                : ImageViewport(
-                    map: _image,
-                    width: (snapshot.data.image.width / window.devicePixelRatio) / _zoomLevel,
-                    height: (snapshot.data.image.height / window.devicePixelRatio) / _zoomLevel,
-                  );
-          },
+        ImageViewport(
+          zoomLevel: _zoomLevel,
         ),
         Row(
           children: <Widget>[
@@ -151,8 +166,7 @@ class ZoomContainerState extends State<ZoomContainer> {
               icon: Icon(Icons.zoom_in),
               onPressed: () {
                 setState(() {
-                  _zoomLevel = _zoomLevel / 2;
-                  _init();
+                  _zoomLevel = _zoomLevel * 2;
                 });
               },
             ),
@@ -164,8 +178,7 @@ class ZoomContainerState extends State<ZoomContainer> {
               icon: Icon(Icons.zoom_out),
               onPressed: () {
                 setState(() {
-                  _zoomLevel = _zoomLevel * 2;
-                  _init();
+                  _zoomLevel = _zoomLevel / 2;
                 });
               },
             ),
@@ -190,7 +203,9 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Move the map"),
       ),
-      body: ZoomContainer(),
+      body: Center(
+        child: ZoomContainer(),
+      ),
     );
   }
 }
